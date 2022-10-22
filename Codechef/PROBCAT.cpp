01bad36451d678f55d1606db0cfc661829afd35e
praveenxpc <109890847+praveenxpc@users.noreply.github.com>
2022-10-22 14:39:36 +0530

    Create  PROBCAT.cpp

---
+++ Codechef/PROBCAT.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int x;
+		cin >> x;
+		if (x < 100) {
+			cout << "Easy" << endl;
+		}
+		else if (x >= 200) {
+			cout << "Hard" << endl;
+		}
+		else {
+			cout << "Medium" << endl;
+		}
+	}
+	return 0;
+}
